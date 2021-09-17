a3257b54915ec4ba48c6a2481bc3b6cfc0acfc81
Erik Halenkamp <58494152+iPBJSammich@users.noreply.github.com>
2021-09-17 15:39:13 -0600

    finishing touches

--- week4/hero/models.py
+++ week4/hero/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Hero(models.Model):
     name = models.CharField(max_length=100)
     url_name = models.CharField(max_length=100)
+    blurb = models.CharField(max_length=100)
 
     def __str__(self):
         return f'{self.name}'
